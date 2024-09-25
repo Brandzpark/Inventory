7d0827eb52f0d225c5c2ec9673668a05a0cbf326
Yohan Dalpethadu <40729152+yohandal123@users.noreply.github.com>
2024-09-25 19:26:14 +0530

    print issue fixed

--- src/app/(authenticated)/purchaseOrders/print/[code]/route.ts
+++ src/app/(authenticated)/purchaseOrders/print/[code]/route.ts
@@ -1,21 +1,29 @@
-
 import { NextResponse, NextRequest } from "next/server";
 import instance from "@/api/base";
-import { printPurchaseOrderApi } from '@/api/purchaseOrder'
+import { printPurchaseOrderApi } from "@/api/purchaseOrder";
 import { notFound, redirect } from "next/navigation";
-
-type ParamsType = { params: { code: string } }
+import puppeteer from "puppeteer";
+type ParamsType = { params: { code: string } };
 export async function GET(request: NextRequest, { params }: ParamsType) {
-    const { code } = params
-    const { data } = await printPurchaseOrderApi({ code })
-    if (!data?.pdfBuffer) {
-        redirect('/404')
-    }
-    const pdfArray: number[] = Object.values(data?.pdfBuffer);
-    const buffer = Buffer.from(pdfArray);
-    return new Response(buffer, {
-        headers: {
-            'Content-Type': 'application/pdf',
-        },
-    })
+  const { code } = params;
+  const { data } = await printPurchaseOrderApi({ code });
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setContent(data?.htmlContent);
+  const pdfBuffer = await page.pdf({
+    format: "A4",
+    printBackground: true,
+  });
+  await browser.close();
+
+  if (pdfBuffer?.length == 0) {
+    redirect("/404");
+  }
+  const pdfArray: number[] = Object.values(pdfBuffer);
+  const buffer = Buffer.from(pdfArray);
+  return new Response(buffer, {
+    headers: {
+      "Content-Type": "application/pdf",
+    },
+  });
 }
