9d84d8bf51484a75cb6a801538f92afdd1877faa
Yohan Dalpethadu <40729152+yohandal123@users.noreply.github.com>
2024-09-25 23:37:17 +0530

    print fix

--- src/app/(authenticated)/purchaseOrders/print/[code]/route.ts
+++ src/app/(authenticated)/purchaseOrders/print/[code]/route.ts
@@ -1,31 +1,19 @@
-import { NextResponse, NextRequest } from "next/server";
-import instance from "@/api/base";
+import { NextRequest } from "next/server";
 import { printPurchaseOrderApi } from "@/api/purchaseOrder";
 import { notFound, redirect } from "next/navigation";
-import puppeteer from "puppeteer";
 type ParamsType = { params: { code: string } };
 export async function GET(request: NextRequest, { params }: ParamsType) {
   const { code } = params;
   const { data } = await printPurchaseOrderApi({ code });
+  const pdfArray: number[] = Object.values(data?.pdfBuffer);
 
-  return Response.json({ success: true, message: "SSR WORKS" });
-  // const browser = await puppeteer.launch();
-  // const page = await browser.newPage();
-  // await page.setContent(data?.htmlContent);
-  // const pdfBuffer = await page.pdf({
-  //   format: "A4",
-  //   printBackground: true,
-  // });
-  // await browser.close();
-
-  // if (pdfBuffer?.length == 0) {
-  //   redirect("/404");
-  // }
-  // const pdfArray: number[] = Object.values(pdfBuffer);
-  // const buffer = Buffer.from(pdfArray);
-  // return new Response(buffer, {
-  //   headers: {
-  //     "Content-Type": "application/pdf",
-  //   },
-  // });
+  if (pdfArray?.length == 0) {
+    redirect("/404");
+  }
+  const buffer = Buffer.from(pdfArray);
+  return new Response(buffer, {
+    headers: {
+      "Content-Type": "application/pdf",
+    },
+  });
 }
